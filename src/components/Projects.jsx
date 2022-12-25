c87d2c3306d5a8a9dec87d9534fc213ab1c18894
Amaya Lucas <amayajlucas@gmail.com>
2022-12-24 21:51:23 -0700

    project page progress

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,8 +1,19 @@
 import snake from "../images/thumbnails/snake.png"
 
-const projectList = [
+const personalProjectList = [
   {
-    title: "_s_n_a_k_e_",
+    title: <p className="text-hero"  style={{textAlign: 'right'}}><span className="text-shino">p</span>o<span className="text-shino">r</span>tfolio</p>,
+    description:
+      "This project recreates the classic web game called Snake using Vanilla Javascript. It uses JS classes and inheritance.",
+    url: "https://_s_n_a_k_e_.surge.sh/",
+    github: "https://github.com/ajluc/_s_n_a_k_e_",
+    image: snake
+  }
+]
+
+const GAProjectList = [
+  {
+    title: <p className="text-hero"  style={{textAlign: 'right'}}><span className="text-shino">s</span>na<span className="text-shino">k</span>e</p>,
     description:
       "This project recreates the classic web game called Snake using Vanilla Javascript. It uses JS classes and inheritance.",
     url: "https://_s_n_a_k_e_.surge.sh/",
@@ -10,14 +21,14 @@ const projectList = [
     image: snake
   },
   {
-    title: "Facadebook",
+    title: <p className="text-hero"  style={{textAlign: 'right'}}>facade<span className="text-shino">b</span>oo<span className="text-shino">k</span></p>,
     description:
       "An anonymous, open source architecture blog where contributors can give their unfiltered opinions of the built environment. Facadebook is an interactive social platform to open the dialogue to all fans (and haters) of architecture.",
     url: "https://facadebook.herokuapp.com/",
     github: "https://github.com/ajluc/facadebook"
   },
   {
-    title: "Potlucky",
+    title: <p className="text-hero"  style={{textAlign: 'right'}}><span className="text-shino">p</span>otluc<span className="text-shino">k</span>y</p>,
     description:
       "A platform for event planning and coordination. Guests can RSVP, indicate what they are bringing, and leave comments.",
     url: "https://feelingpotlucky.herokuapp.com/",
@@ -25,7 +36,7 @@ const projectList = [
 
   },
   {
-    title: "IntDev",
+    title: <p className="text-hero"  style={{textAlign: 'right'}}>intdev</p>,
     description:
       "Take your education into your own hands. IntDev is a platform for academic institutions that allows students to keep track of their transcripts, join classes, and even update their own grades.",
     url: "https://intdev.herokuapp.com/",
@@ -39,18 +50,38 @@ const Projects = () => {
     <div className="dark" id="projects">
       <div className="container">
         <p className="text-hero fade"><span className="text-shino">pr</span>oject<span className="text-shino">s</span></p>
-          <p className="text-header" style={
-            { maxWidth: '500px'}}>General Assembly Projects</p>
-        <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-between'}}>
+        <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-between',  marginTop: '50px'
+}}>
           <div 
-            style={{maxWidth: '500px'}}>
+            style={{maxWidth: '300px', marginRight: '40px'}}>
+          <p className="text-header">General Assembly Projects</p>
             <p>Projects projects these are my projects intro copy to my projects. Projects projects these are my projects intro copy to my projects.Projects projects these are my projects intro copy to my projects</p>
           </div>
-          <div>
-            {projectList.map((project) => (
-            <div key={project.title} style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-between'}}>
-              <p className="text-hero fade">{project.title}</p>
-              <img src={project.image} className="thumbnail" style={{  objectPosition: '100% 0'
+          <div className="project-container">
+            {GAProjectList.map((project) => (
+            <div className="project" key={project.title} style={{display: 'flex', flexDirection: 'row'}}>
+              {project.title}
+              <img src={project.image} className="thumbnail" style={{  objectPosition: '50% 50%', alignSelf: 'flex-end', marginBottom: '.5vw'
+}}/>
+            </div>
+            ))}
+          </div>
+        </div>
+
+
+
+        <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-between',  marginTop: '50px'
+}}>
+          <div 
+            style={{maxWidth: '300px', marginRight: '40px'}}>
+          <p className="text-header">Personal Projects</p>
+            <p>Projects projects these are my projects intro copy to my projects. Projects projects these are my projects intro copy to my projects.</p>
+          </div>
+          <div className="project-container">
+            {personalProjectList.map((project) => (
+            <div className="project" key={project.title} style={{display: 'flex', flexDirection: 'row'}}>
+              {project.title}
+              <img src={project.image} className="thumbnail" style={{  objectPosition: '50% 50%', alignSelf: 'flex-end', marginBottom: '.5vw'
 }}/>
             </div>
             ))}
