10a42b06e8787da27e0ddfa2c0784466ab531e09
Amaya Lucas <amayajlucas@gmail.com>
2022-12-24 23:43:59 -0700

    started popup - very rough

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,25 +5,21 @@ const Header = () => {
         <a href="#home" className="nav-item-link">Home</a>
       </div>
       <div className="nav-item">
-        <p className="nav-item-link">-</p>
       </div>
       <div className="nav-item">
         <a href="#about" className="nav-item-link">About</a>
       </div>
       <div className="nav-item">
-        <p className="nav-item-link">-</p>
       </div>
       <div className="nav-item">
         <a href="#projects" className="nav-item-link">Projects</a>
       </div>
       <div className="nav-item">
-        <p className="nav-item-link">-</p>
       </div>
       <div className="nav-item">
         <a href="#resume" className="nav-item-link">Resume</a>
       </div>
       <div className="nav-item">
-        <p className="nav-item-link">-</p>
       </div>
       <div className="nav-item">
         <a href="#contact" className="nav-item-link">Contact</a>
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,19 +1,22 @@
+import { useState } from "react"
+
 import snake from "../images/thumbnails/snake.png"
+import portfolio from "../images/thumbnails/portfolio.png"
 
 const personalProjectList = [
   {
-    title: <p className="text-hero"  style={{textAlign: 'right'}}><span className="text-shino">p</span>o<span className="text-shino">r</span>tfolio</p>,
+    title: <p className="text-hero link"  style={{textAlign: 'right'}}><span className="text-shino">p</span>o<span className="text-shino">r</span>tfolio</p>,
     description:
       "This project recreates the classic web game called Snake using Vanilla Javascript. It uses JS classes and inheritance.",
     url: "https://_s_n_a_k_e_.surge.sh/",
     github: "https://github.com/ajluc/_s_n_a_k_e_",
-    image: snake
+    image: portfolio
   }
 ]
 
 const GAProjectList = [
   {
-    title: <p className="text-hero"  style={{textAlign: 'right'}}><span className="text-shino">s</span>na<span className="text-shino">k</span>e</p>,
+    title: <p className="text-hero link"  style={{textAlign: 'right'}}><span className="text-shino">s</span>na<span className="text-shino">k</span>e</p>,
     description:
       "This project recreates the classic web game called Snake using Vanilla Javascript. It uses JS classes and inheritance.",
     url: "https://_s_n_a_k_e_.surge.sh/",
@@ -21,14 +24,14 @@ const GAProjectList = [
     image: snake
   },
   {
-    title: <p className="text-hero"  style={{textAlign: 'right'}}>facade<span className="text-shino">b</span>oo<span className="text-shino">k</span></p>,
+    title: <p className="text-hero link"  style={{textAlign: 'right'}}>facade<span className="text-shino">b</span>oo<span className="text-shino">k</span></p>,
     description:
       "An anonymous, open source architecture blog where contributors can give their unfiltered opinions of the built environment. Facadebook is an interactive social platform to open the dialogue to all fans (and haters) of architecture.",
     url: "https://facadebook.herokuapp.com/",
     github: "https://github.com/ajluc/facadebook"
   },
   {
-    title: <p className="text-hero"  style={{textAlign: 'right'}}><span className="text-shino">p</span>otluc<span className="text-shino">k</span>y</p>,
+    title: <p className="text-hero link"  style={{textAlign: 'right'}}><span className="text-shino">p</span>otluc<span className="text-shino">k</span>y</p>,
     description:
       "A platform for event planning and coordination. Guests can RSVP, indicate what they are bringing, and leave comments.",
     url: "https://feelingpotlucky.herokuapp.com/",
@@ -36,7 +39,7 @@ const GAProjectList = [
 
   },
   {
-    title: <p className="text-hero"  style={{textAlign: 'right'}}>intdev</p>,
+    title: <p className="text-hero link"  style={{textAlign: 'right'}}>intdev</p>,
     description:
       "Take your education into your own hands. IntDev is a platform for academic institutions that allows students to keep track of their transcripts, join classes, and even update their own grades.",
     url: "https://intdev.herokuapp.com/",
@@ -46,16 +49,22 @@ const GAProjectList = [
 ]
 
 const Projects = () => {
+  const [open, setOpen] = useState(null)
+  
   return (
     <div className="dark" id="projects">
+      <div className="light project-details collapse">
+        <p>current item</p>
+      </div>
       <div className="container">
         <p className="text-hero fade"><span className="text-shino">pr</span>oject<span className="text-shino">s</span></p>
         <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-between',  marginTop: '50px'
 }}>
           <div 
             style={{maxWidth: '300px', marginRight: '40px'}}>
-          <p className="text-header">General Assembly Projects</p>
-            <p>Projects projects these are my projects intro copy to my projects. Projects projects these are my projects intro copy to my projects.Projects projects these are my projects intro copy to my projects</p>
+          <p className="text-header">General Assembly</p>
+            <p style={{borderTop: '1px solid white', padding: '5px 0px'}}>Twelve-week intensive in Full-Stack Web Development. </p>
+            <p style={{padding: '2px 0px'}}>9/2022 - 12/2022</p>
           </div>
           <div className="project-container">
             {GAProjectList.map((project) => (
@@ -70,12 +79,13 @@ const Projects = () => {
 
 
 
-        <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-between',  marginTop: '50px'
+        <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-between',  marginTop: '70px'
 }}>
           <div 
             style={{maxWidth: '300px', marginRight: '40px'}}>
           <p className="text-header">Personal Projects</p>
-            <p>Projects projects these are my projects intro copy to my projects. Projects projects these are my projects intro copy to my projects.</p>
+            <p style={{borderTop: '1px solid white', padding: '5px 0px'}}>Post-graduation and self-directed. More coming soon</p>
+            <p style={{padding: '2px 0px'}}>12/2022 - present</p>
           </div>
           <div className="project-container">
             {personalProjectList.map((project) => (
