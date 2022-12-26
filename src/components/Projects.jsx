38520587ec71352e16a9c082f676d8d7a48157ce
Amaya Lucas <amayajlucas@gmail.com>
2022-12-25 19:31:47 -0700

    project details progress

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -5,6 +5,7 @@ import portfolio from "../images/thumbnails/portfolio.png"
 
 const personalProjectList = [
   {
+    name: 'portfolio',
     title: <p className="text-hero link"  style={{textAlign: 'right'}}><span className="text-shino">p</span>o<span className="text-shino">r</span>tfolio</p>,
     description:
       "This project recreates the classic web game called Snake using Vanilla Javascript. It uses JS classes and inheritance.",
@@ -16,6 +17,7 @@ const personalProjectList = [
 
 const GAProjectList = [
   {
+    name: 'snake',
     title: <p className="text-hero link"  style={{textAlign: 'right'}}><span className="text-shino">s</span>na<span className="text-shino">k</span>e</p>,
     description:
       "This project recreates the classic web game called Snake using Vanilla Javascript. It uses JS classes and inheritance.",
@@ -24,6 +26,7 @@ const GAProjectList = [
     image: snake
   },
   {
+    name: 'facadebook',
     title: <p className="text-hero link"  style={{textAlign: 'right'}}>facade<span className="text-shino">b</span>oo<span className="text-shino">k</span></p>,
     description:
       "An anonymous, open source architecture blog where contributors can give their unfiltered opinions of the built environment. Facadebook is an interactive social platform to open the dialogue to all fans (and haters) of architecture.",
@@ -31,6 +34,7 @@ const GAProjectList = [
     github: "https://github.com/ajluc/facadebook"
   },
   {
+    name: 'potlucky',
     title: <p className="text-hero link"  style={{textAlign: 'right'}}><span className="text-shino">p</span>otluc<span className="text-shino">k</span>y</p>,
     description:
       "A platform for event planning and coordination. Guests can RSVP, indicate what they are bringing, and leave comments.",
@@ -39,6 +43,7 @@ const GAProjectList = [
 
   },
   {
+    name: 'intdev',
     title: <p className="text-hero link"  style={{textAlign: 'right'}}>intdev</p>,
     description:
       "Take your education into your own hands. IntDev is a platform for academic institutions that allows students to keep track of their transcripts, join classes, and even update their own grades.",
@@ -48,31 +53,54 @@ const GAProjectList = [
   },
 ]
 
+const detailsOpen = () => {
+  let element = document.getElementById('project-details')
+  element.classList.add('open')
+}
+
+const detailsClose = () => {
+  let element = document.getElementById('project-details')
+  element.classList.remove('open')
+}
+
+
 const Projects = () => {
   const [open, setOpen] = useState(null)
   
   return (
     <div className="dark" id="projects">
-      <div className="light project-details collapse">
-        <p>current item</p>
+      <div className="light project-details" id="project-details">
+        <div className="container">
+          <div style={{width: '30vw'}}>
+          {open ? (
+          <div>
+            {open.title}
+            <p>{open.description}</p>
+          </div>
+          ) : <></>}
+          </div>
+          <p onClick={() => detailsClose()}>x</p>
+        </div>
       </div>
       <div className="container">
         <p className="text-hero fade"><span className="text-shino">pr</span>oject<span className="text-shino">s</span></p>
-        <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-between',  marginTop: '50px'
-}}>
+        <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-between',  marginTop: '50px'}}>
           <div 
             style={{maxWidth: '300px', marginRight: '40px'}}>
-          <p className="text-header">General Assembly</p>
+            <p className="text-header">General Assembly</p>
             <p style={{borderTop: '1px solid white', padding: '5px 0px'}}>Twelve-week intensive in Full-Stack Web Development. </p>
             <p style={{padding: '2px 0px'}}>9/2022 - 12/2022</p>
           </div>
+
           <div className="project-container">
             {GAProjectList.map((project) => (
-            <div className="project" key={project.title} style={{display: 'flex', flexDirection: 'row'}}>
-              {project.title}
-              <img src={project.image} className="thumbnail" style={{  objectPosition: '50% 50%', alignSelf: 'flex-end', marginBottom: '.5vw'
-}}/>
-            </div>
+              <div className="project" key={project.name} style={{display: 'flex', flexDirection: 'row'}} onClick={()=>{
+                console.log(project.name)
+                setOpen(project)
+                detailsOpen()}}>
+                {project.title}
+                <img src={project.image} className="thumbnail" style= {{objectPosition: '50% 50%', alignSelf: 'flex-end', marginBottom: '.5vw'}}/>
+              </div>
             ))}
           </div>
         </div>
@@ -83,17 +111,16 @@ const Projects = () => {
 }}>
           <div 
             style={{maxWidth: '300px', marginRight: '40px'}}>
-          <p className="text-header">Personal Projects</p>
+            <p className="text-header">Personal Projects</p>
             <p style={{borderTop: '1px solid white', padding: '5px 0px'}}>Post-graduation and self-directed. More coming soon</p>
             <p style={{padding: '2px 0px'}}>12/2022 - present</p>
           </div>
           <div className="project-container">
             {personalProjectList.map((project) => (
-            <div className="project" key={project.title} style={{display: 'flex', flexDirection: 'row'}}>
-              {project.title}
-              <img src={project.image} className="thumbnail" style={{  objectPosition: '50% 50%', alignSelf: 'flex-end', marginBottom: '.5vw'
-}}/>
-            </div>
+              <div className="project" key={project.name} style={{display: 'flex', flexDirection: 'row'}}>
+                {project.title}
+                <img src={project.image} className="thumbnail" style={{  objectPosition: '50% 50%', alignSelf: 'flex-end', marginBottom: '.5vw'}}/>
+              </div>
             ))}
           </div>
         </div>
