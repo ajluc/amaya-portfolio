5ebceb2e054ac56cbe004fc34e3e7d5cc251ffbc
Amaya Lucas <amayajlucas@gmail.com>
2023-01-08 01:34:24 -0500

    about copy update

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -25,13 +25,12 @@ const About = () => {
                   <p className="text-body">Mobile-First, Responsive Design, Wireframing, ERD, CHD</p>
               </div>
             </div>
-            <p>As a full-stack engineer with a background in architecture, math, and physics, I bring a unique and valuable skill set to the table. My understanding of complex systems and my ability to think critically and creatively make me well-suited to tackle the challenges of modern software development.</p>
+            <p>Hey there! I am an upcoming web developer with a passion for learning and a natural curiosity. I am a creative problem solver as well as an analytical thinker.</p>
             <br/>
-            <p>My background in architecture has given me an eye for design and a strong foundation in problem-solving, both of which help me to build and maintain web applications. My math and physics training have honed my analytical skills and my ability to understand and manipulate complex data, making me a valuable asset when it comes to developing algorithms and data-driven solutions.</p>
+            <p>My past experience has made me an adaptable programmer: I have a strong technical background with an undergraduate degree in mathematics and physics that gives me confidence in my ability to break down and solve tough challenges; meanwhile, project management during my years in architecture taught me how to balance big picture goals while still meeting challenging deadlines. </p>
             <br/>
-            <p>In my role as a full-stack engineer, I will have the opportunity to work on a wide range of projects, from front-end user interfaces to back-end servers and databases. I will be responsible for designing, building, and maintaining scalable and robust systems, collaborating with cross-functional teams to ensure that all solutions meet the needs of my users.</p>
-            <br/>
-            <p>With my unique blend of skills and experience, I have the potential to make a significant impact in the field of software development. I have no doubt that I will thrive and make a valuable contribution to any team.</p>
+            <p>I am perpetually learning and leveling up, one day at a time. If you're interested in getting to know me, please <a href="#contact" style={{fontWeight: "500"}}>reach out!</a>
+</p>
           </div>
         </div>
       </div>
